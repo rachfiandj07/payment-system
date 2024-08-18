40ad4bdb081d87b1d633396437321e2c71167b6f
rachfiandj07 <naufalrdj@gmail.com>
2024-08-19 02:03:04 +0700

    init base specs for features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, InvoiceModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
