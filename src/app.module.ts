759faa70f772ba64c8db884c0240ef4c2cd87ef9
rachfiandj07 <naufalrdj@gmail.com>
2024-08-19 04:56:08 +0700

    feat(ConfigModule): add config module for adding .env in project

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AdminModule } from './admin/admin.module';
 import { InvoiceModule } from './invoice/invoice.module';
 import { CustomerModule } from './customer/customer.module';
 import { AuthorizationModule } from './authorization/authorization.module';
+import { PaymentModule } from './payment/payment.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AdminModule, InvoiceModule, CustomerModule, AuthorizationModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true}),
+    AdminModule, InvoiceModule, CustomerModule, AuthorizationModule, PaymentModule, DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
